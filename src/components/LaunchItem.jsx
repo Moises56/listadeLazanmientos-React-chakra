f3db000fb9b7851f281e756c49bb0cad093a3302
Moises Aviles <mougriond1@gmail.com>
2022-09-02 00:59:37 -0600

    details

--- src/components/LaunchItem.jsx
+++ src/components/LaunchItem.jsx
@@ -1,5 +1,5 @@
 import { HiCalendar } from "react-icons/hi";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Box, Flex, Text, Spacer, Tag, Button, Icon } from "@chakra-ui/react";
 
 import dayjs from 'dayjs';
@@ -18,8 +18,8 @@ return (
             <Tag p={2} variantColor="teal" colorScheme={launch.launch_success ? 'green' : 'red' }>
                 {launch.launch_success ? 'Success' : 'Failure'}
             </Tag>
-
         </Flex>
+
         <Flex align="center">
             <Icon as={HiCalendar} color="gray.500" /> 
             <Text fontSize="sm" ml={1} color="gray.500">
@@ -31,13 +31,15 @@ return (
             </Text>
         </Flex>
 
-    <Button mt={4} 
-            variantColor="teal" 
-            colorScheme="purple" 
-            size="lg" 
-            variant="solid">
-        More Details    
-    </Button>
+    <Link to={`/launch/${launch.flight_number}`}>
+        <Button mt={4} 
+                variantColor="teal" 
+                colorScheme="purple" 
+                size="lg" 
+                variant="solid">
+            More Details    
+        </Button>
+    </Link>
 
     </Box>
 );
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 // 1. import `ChakraProvider` component
 import { ChakraProvider } from '@chakra-ui/react'
+// 2. react router
+import { BrowserRouter } from "react-router-dom";
 
 import { App } from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>
 )
